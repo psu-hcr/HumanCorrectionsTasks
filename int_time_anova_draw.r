829a4f3b093250c32971141b6ae3a8d6f67b930d
QAndersonWatson <93205729+QAndersonWatson@users.noreply.github.com>
2024-03-27 18:04:38 -0400

    Adding interaction time ANOVAs

---
+++ int_time_anova_draw.r
@@ -0,0 +1,24 @@
+library(pwr)
+
+#sink data to text file output
+sink("/home/qxa5031/bagfiles_Quentin_copy/p_values_draw.txt")
+#import csv into r
+p_data <- read.csv("/home/qxa5031/bagfiles_Quentin_copy/draw_data.csv")
+#attach the data to the script so we can use the headers to call data sets
+df <- attach(p_data)
+#assign our factors
+subj <-factor(Participant)
+error <-factor(Error_Type)
+trial <-factor(Trial)
+
+print("Anova Results for the Angle of Motion")
+#compute anova for angle measures
+
+#model1 <- aov(abs_angle_error ~ (con*ang*mag) + Error(subj/(con*ang*mag)))
+#print(summary(model1))
+
+model_1 <- aov(Interaction_Time ~ (error*trial) + Error(subj/error))
+atest <- summary(model_1)
+astructure <- str(atest)
+print(atest)
+boxplot(Interaction_Time ~ (error * trial), data=, xlab="ErrorType/Trial", ylab = "Interaction_Time")
